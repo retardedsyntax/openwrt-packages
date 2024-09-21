b046baeebef684e97106d1f1f4f31fdc76bbe93c
Joonas Onatsu <joonas@onatsu.net>
2024-09-22 00:09:27 +0300

    linting with shellcheck

--- uqmi/files/usr/bin/uqmi_d.sh
+++ uqmi/files/usr/bin/uqmi_d.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# shellcheck shell=busybox
 #
 # uqmi daemon, runs every 30sec
 # 1. Check IP-address in get-current-settings
@@ -17,18 +18,18 @@ sendFolder=/var/sms/send
 mkdir -p $receiveFolder
 mkdir -p $sendFolder
 
-interface=$(uci show network | grep qmi | awk -F . '{print $2}')
-device=$(uci get network.${interface}.device)
-default_profile=$(uci get network.${interface}.default_profile)
-ipv6profile=$(uci get network.${interface}.ipv6profile)
-smsc=$(uci get network.${interface}.smsc)
+interface="$(uci show network | grep qmi | awk -F . '{print $2}')"
+device="$(uci get network.${interface}.device)"
+default_profile="$(uci get network.${interface}.default_profile)"
+ipv6profile="$(uci get network.${interface}.ipv6profile)"
+smsc="$(uci get network.${interface}.smsc)"
 
 json_load "$(ubus call network.interface.${interface} status)"
 json_get_var ifname l3_device
 json_select data
 json_get_vars cid_4 pdh_4 cid_6 pdh_6 zone
 
-if [ ! -n "$pdh_4" ] && [ ! -n "$pdh_6" ]
+if [ -z "$pdh_4" ] && [ -z "$pdh_6" ]
 then
 	/etc/init.d/uqmi_d stop 2> /dev/null
 fi
@@ -40,37 +41,37 @@ do
 # Check wwan connectivity
 	if [ -n "$pdh_4" ]
 	then
-		ipv4connected="$(uqmi -s -d $device --set-client-id wds,$cid_4 --get-current-settings)"
+		ipv4connected="$(uqmi -s -d "$device" --set-client-id wds,"$cid_4" --get-current-settings)"
 	fi
 	if [ -n "$pdh_6" ]
 	then
-		ipv6connected="$(uqmi -s -d $device --set-client-id wds,$cid_6 --get-current-settings)"
+		ipv6connected="$(uqmi -s -d "$device" --set-client-id wds,"$cid_6" --get-current-settings)"
 	fi
 
 	if [ "$ipv4connected" = '"Out of call"' ] || [ "$ipv6connected" = '"Out of call"' ]
 	then
-		logger -t uqmi_d Modem disconnected
+		logger -t uqmi_d 'Modem disconnected'
 		proto_init_update "$ifname" 0
 		proto_send_update "$interface"
 
 # IPv4
 		if [ -n "$pdh_4" ]
 		then
-			uqmi -s -d $device --set-client-id wds,"$cid_4" \
+			uqmi -s -d "$device" --set-client-id wds,"$cid_4" \
 				--release-client-id wds
 
-			cid_4=$(uqmi -s -d $device --get-client-id wds)
+			cid_4="$(uqmi -s -d "$device" --get-client-id wds)"
 			uqmi -s -d "$device" --set-client-id wds,"$cid_4" --set-ip-family ipv4
-			pdh_4=$(uqmi -s -d $device --set-client-id wds,"$cid_4" \
+			pdh_4="$(uqmi -s -d "$device" --set-client-id wds,"$cid_4" \
 				--start-network \
-				--profile $default_profile)
+				--profile $default_profile)"
 			if [ "$pdh_4" = '"Call failed"' ]
 			then
 				logger -t uqmi_d 'Unable to re-connect IPv4 - Interface restarted'
 				ifup $interface
 				/etc/init.d/uqmi_d stop
 			else
-				logger -t uqmi_d IPv4 re-connected
+				logger -t uqmi_d 'IPv4 re-connected'
 			fi
 			proto_init_update "$ifname" 1
 			proto_set_keep 1
@@ -81,7 +82,7 @@ do
 			proto_close_data
 			proto_send_update "$interface"
 
-			json_load "$(uqmi -s -d $device --set-client-id wds,$cid_4 --get-current-settings)"
+			json_load "$(uqmi -s -d "$device" --set-client-id wds,"$cid_4" --get-current-settings)"
 			json_select ipv4
 			json_get_var ip_4 ip
 			json_get_var gateway_4 gateway
@@ -104,24 +105,24 @@ do
 # IPv6
 		if [ -n "$pdh_6" ]
 		then
-			uqmi -s -d $device --set-client-id wds,"$cid_6" \
+			uqmi -s -d "$device" --set-client-id wds,"$cid_6" \
 				--release-client-id wds
 
-			cid_6=$(uqmi -s -d $device --get-client-id wds)
+			cid_6="$(uqmi -s -d "$device" --get-client-id wds)"
 			uqmi -s -d "$device" --set-client-id wds,"$cid_6" --set-ip-family ipv6
 			if [ -n "$pdh_4" ] && [ -n "$pdh_6" ]
 			then
-				pdh_6=$(uqmi -s -d $device --set-client-id wds,"$cid_6" \
-					--start-network)
+				pdh_6="$(uqmi -s -d "$device" --set-client-id wds,"$cid_6" \
+					--start-network)"
 			elif [ -n "$ipv6profile" ]
 			then
-				pdh_6=$(uqmi -s -d $device --set-client-id wds,"$cid_6" \
+				pdh_6="$(uqmi -s -d "$device" --set-client-id wds,"$cid_6" \
 					--start-network \
-					--profile $ipv6profile)
+					--profile "$ipv6profile")"
 			else
-				pdh_6=$(uqmi -s -d $device --set-client-id wds,"$cid_6" \
+				pdh_6="$(uqmi -s -d "$device" --set-client-id wds,"$cid_6" \
 					--start-network \
-					--profile $default_profile)
+					--profile "$default_profile")"
 			fi
 			if [ "$pdh_6" = '"Call failed"' ]
 			then
@@ -129,7 +130,7 @@ do
 				ifup $interface
 				/etc/init.d/uqmi_d stop
 			else
-				logger -t uqmi_d IPv6 re-connected
+				logger -t uqmi_d 'IPv6 re-connected'
 			fi
 			proto_init_update "$ifname" 1
 			proto_set_keep 1
@@ -140,7 +141,7 @@ do
 			proto_close_data
 			proto_send_update "$interface"
 
-			json_load "$(uqmi -s -d $device --set-client-id wds,$cid_6 --get-current-settings)"
+			json_load "$(uqmi -s -d "$device" --set-client-id wds,"$cid_6" --get-current-settings)"
 			json_select ipv6
 			json_get_var ip_6 ip
 			json_get_var gateway_6 gateway
@@ -170,17 +171,17 @@ do
 	smsTOsend=$(ls $sendFolder -w 1 | sed -n '1p')
 	while [ -n "$smsTOsend" ]
 	do
-		Bnumber=$(sed -n '1p' $sendFolder/$smsTOsend)
-		SMStext=$(sed -n '2,$p' $sendFolder/$smsTOsend)
-		logger -t uqmi_d SMS sent to $Bnumber
+		Bnumber="$(sed -n '1p' $sendFolder/$smsTOsend)"
+		SMStext="$(sed -n '2,$p' $sendFolder/$smsTOsend)"
+		logger -t uqmi_d "SMS sent to $Bnumber"
 		if [ -z "$smsc" ]
 		then
-			uqmi -d $device --send-message "$SMStext" \
-							--send-message-target $Bnumber
+			uqmi -d "$device" --send-message "$SMStext" \
+							--send-message-target "$Bnumber"
 		else
-			uqmi -d $device --send-message "$SMStext" \
-							--send-message-target $Bnumber \
-							--send-message-smsc $smsc
+			uqmi -d "$device" --send-message "$SMStext" \
+							--send-message-target "$Bnumber" \
+							--send-message-smsc "$smsc"
 		fi
 		rm $sendFolder/$smsTOsend
 		smsTOsend=$(ls $sendFolder -w 1 | sed -n '1p')
@@ -190,10 +191,10 @@ do
 # Check received SMS
 	for storage in sim me
 	do
-		messageID=$(uqmi -d $device --list-messages --storage $storage  | jsonfilter -e '@[0]')
+		messageID="$(uqmi -d "$device" --list-messages --storage "$storage" | jsonfilter -e '@[0]')"
 		while [ -n "$messageID" ]
 		do
-			json_load "$(uqmi -s -d $device --get-message $messageID --storage $storage 2>/dev/null)"
+			json_load "$(uqmi -s -d "$device" --get-message "$messageID" --storage "$storage" 2>/dev/null)"
 			json_get_var smsc smsc
 			json_get_var sender sender
 			json_get_var timestamp timestamp
@@ -202,34 +203,32 @@ do
 			json_get_var concat_parts concat_parts
 			json_get_var text text
             json_get_var ucs2 ucs-2
-			timestamp=$(echo $timestamp | sed -e 's/-//g' | sed -e 's/://g' | sed -e 's/ /T/g')
+			timestamp="$(echo $timestamp | sed -e 's/-//g' | sed -e 's/://g' | sed -e 's/ /T/g')"
 			if [ -n "$concat_ref" ]
 			then
-				sms_file=sms_${timestamp}_${concat_ref}_${concat_part}_${concat_parts}
+				sms_file="sms_${timestamp}_${concat_ref}_${concat_part}_${concat_parts}"
 			else
-				sms_file=sms_${timestamp}
+				sms_file="sms_${timestamp}"
 			fi
-			echo "$sender" > $receiveFolder/$sms_file
+			echo "$sender" > "$receiveFolder/$sms_file"
             if [ -n "$text" ]
    			then
-				echo "$text" >> $receiveFolder/$sms_file
+				echo "$text" >> "$receiveFolder/$sms_file"
 			fi
 			if [ -n "$ucs2" ]
 			then
-				echo '"'$ucs2'"' | sed 's/\([0-9A-F]\{4\}\)/\\\u\1/gI' | jsonfilter -e '@' >> $receiveFolder/$sms_file
+				echo '"'$ucs2'"' | sed 's/\([0-9A-F]\{4\}\)/\\\u\1/gI' | jsonfilter -e '@' >> "$receiveFolder/$sms_file"
 			fi
-			logger -t uqmi_d SMS received from $sender
-			/usr/bin/uqmi_sms.sh $receiveFolder/${sms_file} 2> /dev/null
-			uqmi -d $device --delete-message $messageID --storage $storage
+			logger -t uqmi_d "SMS received from $sender"
+			/usr/bin/uqmi_sms.sh "$receiveFolder/$sms_file" 2> /dev/null
+			uqmi -d "$device" --delete-message "$messageID" --storage "$storage"
 			sleep 1
-			messageID=$(uqmi -d $device --list-messages --storage $storage  | jsonfilter -e '@[0]')
+			messageID=$(uqmi -d "$device" --list-messages --storage "$storage"  | jsonfilter -e '@[0]')
 		done
 	done
 
 # LED trigger
-	rssi=$(uqmi -s -d $device --get-signal-info | jsonfilter -e '@["rssi"]')
+	rssi="$(uqmi -s -d "$device" --get-signal-info | jsonfilter -e '@["rssi"]')"
 	/usr/bin/uqmi_led.sh "$rssi" 2> /dev/null
-
-
 	sleep 30
 done
